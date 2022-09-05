30ba43d7f2a43d212f653a8ea709a03c4b588162
ASHOKROCK <93025555+ASHOKROCK@users.noreply.github.com>
2022-09-05 23:03:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "172.16.0.0/16"
+resource "aws_vpc" "new_vpc" {
+  cidr_block = "172.20.0.0/16"
 
   tags = {
     Name = "tf-example"
@@ -7,8 +7,8 @@ resource "aws_vpc" "my_vpc" {
 }
 
 resource "aws_subnet" "my_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "172.16.10.0/24"
+  vpc_id            = aws_vpc.new_vpc.id
+  cidr_block        = "172.20.10.0/24"
   availability_zone = "us-east-1a"
 
   tags = {
@@ -16,8 +16,8 @@ resource "aws_subnet" "my_subnet" {
   }
 }
 
-resource "aws_vpc" "my_vpc" {
-  availability_zone = "us-east-1a"
+resource "aws_vpc" "custom_vpc" {
+  cidr_block = "172.23.0.0/16"
 
   tags = {
     Name = "Create subnet for us-east-1a"
